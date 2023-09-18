335580c12bd62146fadc8e2863d6dcbbceaaede9
nihayaturrahmah27 <afspraak.hyrm@gmail.com>
2023-09-18 22:22:53 +0700

    edit widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Aplikasi Flutter Pertama",
+        title: "Flutter Pertemuan 2",
         home: Scaffold(
             appBar: AppBar(
           title: Text("Flutter Pertemuan 2"),
